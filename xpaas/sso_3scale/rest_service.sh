5eed3778eb81053fb5aa54877a366d019b492e83
Karsten Gresch <git-2016@gresch.io>
2017-07-10 21:00:46 +0200

    Changed during playthrough...

--- xpaas/sso_3scale/rest_service.sh
+++ xpaas/sso_3scale/rest_service.sh
@@ -2,4 +2,4 @@
 # assuming running on minishift
 oc login -u developer -p developer --insecure-skip-tls-verify=true
 oc new-project rest-services --display-name="rest-services-ola"
-oc new-app service-ola openshift/redhat-openjdk18-openshift:1.1~https://github.com/redhat-helloworld-msa/ola.git 
+oc new-app openshift/redhat-openjdk18-openshift:1.1~https://github.com/redhat-helloworld-msa/ola.git 
